84396546d27b1665c673e7af7ceaa80a923f4107
Rafael Tavares <rafatavares03@hotmail.com>
2022-08-09 11:10:14 -0300

    decrypting messages

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const cryptoKeys = {
   o: 'ober',
   u: 'ufat',
 }
+let encrypted = false;
 
 function writeText(text) {
   const res = document.querySelector('#result');
@@ -20,6 +21,27 @@ function encrypt() {
     }
   }
   writeText(text);
+  encrypted = true;
+}
+
+function decrypt() {
+  var text;
+  if (encrypted) {
+    text = document.querySelector('#result').innerText;
+    console.log(text, encrypted, 'result');
+  } else {
+    text = document.querySelector('#text').value;
+    console.log(text, encrypted, 'textarea');
+  }
+  for(key in cryptoKeys) {
+    if(text.indexOf(cryptoKeys[key]) >= 0) {
+      let regex = new RegExp(cryptoKeys[key], 'gi');
+      text = text.replaceAll(regex, key);
+    }
+  }
+  writeText(text);
+  encrypted = false;
 }
 
 document.querySelector('#encrypt').addEventListener('click', encrypt);
+document.querySelector('#decrypt').addEventListener('click', decrypt);
